e05c6435e2cb2a31df0df32bb05e857c56a840f5
Piyushnmj <piyushnmj@yahoo.com>
2022-12-09 17:00:49 +0530

    Welcome

---
+++ CollectionDemoProblems/CollectionDemoProblems/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CollectionDemoProblems
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Collection Demo Classes");
+        }
+    }
+}
